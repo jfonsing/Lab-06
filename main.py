0557dbb3f15513f439e5a65c0de0abd1902ea666
PerlHP <create@hpdev.co>
2023-10-25 14:29:25 -0400

    added decoder function and comments

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+'''
+    Author: jfonsing
+    Date: 10/25/2023
+    Project: Lab 6: Git
+    Description: A lab demonstrating the purpose and usefullness of version control
+'''
+
+# defines main function
 def main():
     option = -1
     password = ""
-    new_pass = ""
+    new_pass = 0
+    decoded_pass = 0
+
     while option != 3:
         print("Menu")
         print("-------------")
@@ -12,18 +22,24 @@ def main():
         option = int(input("Please enter an option: "))
 
         if option == 1:
-            password = int(input("Please enter you password to encode: "))
+            password = input("Please enter you password to encode: ")
+            new_pass = encode(int(password))
             print("Your password has been encoded and stored!")
         elif option == 2:
-            new_pass = encode(password)
-            print(f'The encoded password is {new_pass}, and the original password is {decode(new_pass)}.')
+            decoded_pass = decode(new_pass)
+            print(f'The encoded password is {new_pass}, and the original password is {password}.')
         elif option == 3:
             break
 
+# defines encode function
+# accepts a number and returns an encoded password
 def encode(passw):
     return passw + 33333333
 
+# defines decode function
+# accepts a number and returns a decoded password
 def decode(pass_new):
-    return pass_new - 33333333
+    decoded_val = pass_new - 33333333
+    return decoded_val
 
 main()
