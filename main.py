83c2a3ac3d617ee2411ac5b23ed9f7971fd1d897
Jenna Fonsing <jfonsing@ufl.edu>
2023-10-25 14:16:11 -0400

    added encoder function

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello world")
+def main():
+    option = -1
+    password = ""
+    new_pass = ""
+    while option != 3:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+        option = int(input("Please enter an option: "))
+
+        if option == 1:
+            password = int(input("Please enter you password to encode: "))
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            new_pass = encode(password)
+            print(f'The encoded password is {new_pass}, and the original password is {decode(new_pass)}.')
+        elif option == 3:
+            break
+
+def encode(passw):
+    return passw + 33333333
+
+def decode(pass_new):
+    return pass_new - 33333333
+
+main()
